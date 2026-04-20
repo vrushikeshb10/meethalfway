7a59688791cb35ff641c7d681b8e4942f5bd5a74
Vrushikesh <vrushikesh@admins-MacBook-Pro.local>
2026-04-20 21:50:19 +0530

    feat: initialize Next.js project with Tailwind, Jest, and dependencies

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const nextJest = require('next/jest')
+const createJestConfig = nextJest({ dir: './' })
+module.exports = createJestConfig({
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/']
+})
